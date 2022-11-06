704c84d15423b938ee4a551b7d37a9fdd13c883e
azarasys <azarasys@gmail.com>
2022-11-06 09:20:22 +0900

    更新と削除関数追加と微修正

--- app/layer/secrets_manager.py
+++ app/layer/secrets_manager.py
@@ -1,47 +1,72 @@
-import ast
 import boto3
 import base64
+import json
 import os
 from datetime import datetime
 from botocore.exceptions import ClientError
 
 REGION_NAME = os.environ['REGION_NAME']
+RECOVERY_WINDOW_IN_DAYS = os.environ['RECOVERY_WINDOW_IN_DAYS']
 
 def get_secret(secret_name: str):
-    session = boto3.sessin.Session()
+    ''' シークレットキーを取得する
+    '''
+    session = boto3.session.Session()
     client = session.client(service_name='secretsmanager')
 
     try:
         response = client.get_secret_value(SecretId=secret_name)
     except ClientError as e:
-        if e.response['Error']['Code'] == 'ResourceNotFoundException':
-            print("The requested secret " + secret_name + " was not found")
-        elif e.response['Error']['Code'] == 'InvalidRequestException':
-            print("The request was invalid due to:", e)
-        elif e.response['Error']['Code'] == 'InvalidParameterException':
-            print("The request had invalid params:", e)
-        elif e.response['Error']['Code'] == 'DecryptionFailure':
-            print("The requested secret can't be decrypted using the provided KMS key:", e)
-        elif e.response['Error']['Code'] == 'InternalServiceError':
-            print("An error occurred on service side:", e)
+        print(f'Couldnt fetch secret {secret_name}: {e}')
     else:
+        print(f'Get secrets {secret_name}')
         if 'SecretString' in response:
-            secret = response['SecretString']
+            return response['SecretString']
         else:
-            secret = base64.b64decode(response['SecretBinary'])
-    return ast.literal_eval(secret)
+            return base64.b64decode(response['SecretBinary'])
 
-def create_secret(secret_name: str, secret_value: str):
+def create_secret(secret_name: str, secret_value: dict):
+    ''' キーペアのシークレットキーを作成する
+    '''
     session = boto3.session.Session()
     client = session.client(service_name='secretsmanager')
-    now = datetime.now()
     try:
         response = client.create_secret(
             Name=secret_name,
-            Description=f'Create at {now.strftime("%Y-%m-%d %H:%M%S")}',
-            SecretString=secret_value
+            Description=f'Create at {datetime.now().strftime("%Y-%m-%d %H:%M%S")}',
+            SecretString=json.dumps(secret_value)
         )
     except ClientError as e:
         print(f'Couldnt get secret {secret_name}: {e}')
     else:
         print(f'Created secret {secret_name}.')
+
+def update_secret(secret_name: str, secret_value: str):
+    ''' キーペアのシークレットキーを更新する
+    '''
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager')
+    try:
+        response = client.update_secret(
+            SecretId=secret_name,
+            SecretString=json.dumps(secret_value)
+        )
+    except ClientError as e:
+        print(f'Couldnt upgrade secret {secret_name}: {e}')
+    else:
+        print(f'Updated secret {secret_name}.')
+
+def delete_secret(secret_name: str):
+    ''' シークレットキーを更新する
+    '''
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager')
+    try:
+        response = client.delete_secret(
+            SecretId=secret_name,
+            RecoveryWindowInDays=RECOVERY_WINDOW_IN_DAYS
+        )
+    except ClientError as e:
+        print(f'Couldnt delete secret {secret_name}: {e}')
+    else:
+        print(f'Deleted secret {secret_name}.')
