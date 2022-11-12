0dddc72d04a0d63382f7dd86f98364cd7e1f31dd
azarasys <azarasys@gmail.com>
2022-11-12 17:30:30 +0900

    全体的に修正

--- app/lambda/line_login/main.py
+++ app/lambda/line_login/main.py
@@ -3,36 +3,55 @@ import requests
 import boto3
 from layer.dynamodb import get_service_by_id, put_data
 from layer.secrets_manager import get_secret
-from layer.notice import send_user_register_form_url
+from layer.notice import send_line_push_massage, generate_body
 
 LINE_API_URL_AUTH = os.environ['LINE_API_URL_AUTH']
 LINE_API_URL_VERIFY = os.environ['LINE_API_URL_VERIFY']
 TOKEN_TABLE_NAME = os.environ['TOKEN_TABLE_NAME']
 LOGIN_COMPLITE_URL = os.environ['LOGIN_COMPLITE_URL']
+REGISTER_FORM_URL = os.environ['REGISTER_FORM_URL']
 
 def __get_channel_id(event: dict) -> str:
-    # リクエスト元のを取得
+    '''リクエスト元のを取得
+    '''
     service_id = event['requestContext']['stage']
     return get_service_by_id(service_id)
 
 def __get_host_uri(event: dict) -> str:
-    # リクエスト元のURIを取得
+    '''リクエスト元のURIを取得
+    '''
     host = event['headers']['Host']
     path = event['path']
     return f'{host}/{path}'
 
-def __put_user_token(user_id: str, token: str, channel_id: str, expire_at: int):
+def __put_user_token(line_id: str, token: str, channel_id: str, expire_at: int):
+    '''ユーザ認証トークン登録
+    '''
     is_put_token = put_data(
         table_name=TOKEN_TABLE_NAME,
         item={
-            'UserId': user_id,
-            'Token': token,
-            'ChannelId': channel_id,
-            'ExpireAt': expire_at
+            'line_id': line_id,
+            'token': token,
+            'channel_id': channel_id,
+            'expire_at': expire_at
         }
     )
     if not is_put_token:
         raise Exception('Could not put token.')
+
+def __generate_massage_params(channel_id: str, line_id: str):
+    '''送信用メッセージパラメータ作成
+    '''
+    return {
+        'line_id': line_id,
+        'channel_id': channel_id,
+        'body1_id': '100',
+        'body1_args': [REGISTER_FORM_URL],
+        'body2_id': '',
+        'body2_args': '',
+        'body3_id': '',
+        'body3_args': ''
+    }
 
 def lambda_handler(event, context):
     channel_id = __get_channel_id(event)
@@ -58,19 +77,21 @@ def lambda_handler(event, context):
     ).json()
     verify_res.update(data)
 
+    
+    line_id =  verify_res['sub'],
+    token =  auth_res['id_token'],
+    expire_at =  verify_res['exp']
+
     # ユーザ認証トークン登録
     __put_user_token(
-        user_id=verify_res['sub'],
-        token=auth_res['id_token'],
+        user_id=line_id,
+        token=token,
         channel_id=channel_id,
-        expire_at=verify_res['exp']
+        expire_at=expire_at
     )
 
     # 会員登録フォーム発行
-    send_user_register_form_url(
-        channel_id=channel_id,
-        user_id=verify_res['sub']
-    )
+    send_line_push_massage(__generate_massage_params(channel_id, line_id))
 
     return {
         'statusCode': 302,
--- app/layer/notice.py
+++ app/layer/notice.py
@@ -17,43 +17,43 @@ s3 = boto3.resorce('s3')
 sns = boto3.resorce('sns')
 bucket = s3.Bucket(S3_CONFIG_BUCKET_NAME)
 
-def __generate_subject(params: dict):
+def generate_subject(params: dict):
     subject_template = json.loads(bucket.Object(MESSAGE_SUBJECT_TEMPLATE_JSON_PATH).get()['Body'].decode('utf-8'))
     subject1 = ''
     subject2 = ''
     subject3 = ''
     
-    subject1_id = params.get('SUBJECT1_ID')
+    subject1_id = params.get('subject1_id')
     if subject1_id:
-        subject1 = subject_template.get(subject1_id).format(params.get('SUBJECT1_ARGS'))
+        subject1 = subject_template.get(subject1_id).format(params.get('subject1_args'))
 
-    subject2_id = params.get('SUBJECT2_ID')
+    subject2_id = params.get('subject2_id')
     if subject2_id:
-        subject2 = subject_template.get(subject2_id).format(params.get('SUBJECT2_ARGS'))
+        subject2 = subject_template.get(subject2_id).format(params.get('subject2_args'))
 
-    subject3_id = params.get('SUBJECT3_ID')
+    subject3_id = params.get('subject3_id')
     if subject3_id:
-        subject3 = subject_template.get(subject3_id).format(params.get('SUBJECT3_ARGS'))
+        subject3 = subject_template.get(subject3_id).format(params.get('subject3_args'))
 
     return f'{subject1}{subject2}{subject3}'
 
-def __generate_body(params: dict):
+def generate_body(params: dict):
     body_template = json.loads(bucket.Object(MESSAGE_BODY_TEMPLATE_JSON_PATH).get()['Body'].decode('utf-8'))
     body1 = ''
     body2 = ''
     body3 = ''
     
-    body1_id = params.get('BODY1_ID')
+    body1_id = params.get('body1_id')
     if body1_id:
-        body1 = body_template.get(body1_id).format(params.get('BODY1_ARGS'))
+        body1 = body_template.get(body1_id).format(params.get('body1_args'))
 
-    body2_id = params.get('BODY2_ID')
+    body2_id = params.get('body2_id')
     if body2_id:
-        body2 = body_template.get(body2_id).format(params.get('BODY2_ARGS'))
+        body2 = body_template.get(body2_id).format(params.get('body2_args'))
 
-    body3_id = params.get('BODY3_ID')
+    body3_id = params.get('body3_id')
     if body3_id:
-        body3 = body_template.get(body3_id).format(params.get('BODY3_ARGS'))
+        body3 = body_template.get(body3_id).format(params.get('body3_args'))
 
     return '\n\n'.join([body for body in [body1, body2, body3] if body])
 
@@ -61,16 +61,16 @@ def send_sms(params: dict):
     # いつか実装する
     pass
 
-def send_line(params: dict):
+def send_line_push_massage(params):
     request_json = {
-        'to': params['LineId'],
+        'to': params['line_id'],
         'messages': {
             'type': 'text',
-            'text': params['Body']
+            'text': generate_body(params)
         }
     }
     
-    channel_secret = get_secret(params['ChannelId'])
+    channel_secret = get_secret(params['channel_id'])
 
     headers = {
         'Authorization': f'Bearer {channel_secret}',
@@ -96,15 +96,15 @@ def send_message(params: dict):
     print('send message start.')
 
     # 件名を組み立てる
-    params['Subject'] = __generate_subject(params)
+    params['Subject'] = generate_subject(params)
     print('complete generate subject.')
     # 本文を組み立てる
-    params['Body']  = __generate_body(params)
+    params['Body']  = generate_body(params)
     print('complete generate body.')
     # 送信する
 
     if 'LINE' in params['Notice'].keys():
-        send_line(params)
+        send_line_push_massage(params)
         print('send line.')
     if 'EMAIL' in params['Notice'].keys():
         send_email(params)
